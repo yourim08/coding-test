935953913197c1c1b9a151398c500401c4761bc9
전유림 <165138502+JeonYuLIm0311@users.noreply.github.com>
2024-04-07 23:16:17 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.02 ms, Memory: 87.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = 1;
+        int answer1 = -1;
+        if(num1==num2){
+        return answer;
+        }
+        else{
+        return answer1;
+        }
+    }
+}
