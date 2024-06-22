501bfa1cbed653c282a5e7c88325d606ab88ef85
전유림 <165138502+JeonYuLIm0311@users.noreply.github.com>
2024-06-23 03:34:39 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.02 ms, Memory: 78.6 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        int temp = 0;
+        for(int i=0; i<array.length-1; i++){
+            for(int j=i+1;j<array.length;j++){
+                if(array[i]>array[j]){
+                    temp=array[i];
+                    array[i]=array[j];
+                    array[j]=temp;
+                } 
+            }
+        }
+        answer = array[array.length/2];
+        return answer;
+    }
+}
