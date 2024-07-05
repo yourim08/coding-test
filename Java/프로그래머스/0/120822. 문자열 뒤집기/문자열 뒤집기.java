f7e7825be5cf223de8a4d6200247f326be3e7c84
전유림 <165138502+JeonYuLIm0311@users.noreply.github.com>
2024-07-05 10:00:32 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.03 ms, Memory: 73 MB -BaekjoonHub

--- Java/프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
+++ Java/프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -1,10 +1,10 @@
 class Solution {
     public String solution(String my_string) {
-        char sc[] = new char[my_string.length()];
-        for(int i=0; i<sc.length ;i++){
-            sc[(sc.length-1)-i]= my_string.charAt(i);
+        char ch[] = new char[my_string.length()];
+        for(int i=0 ; i<ch.length ; i++){
+            ch[(ch.length-1)-i]=my_string.charAt(i);
         }
-        String answer= String.valueOf(sc);
+        String answer = String.valueOf(ch);
         return answer;
     }
 }
