231574e89f5a4e00f7f3a73d58f2d9b5e2a7d09d
전유림 <165138502+yourim08@users.noreply.github.com>
2024-07-30 14:36:57 +0900

    [level 0] Title: 세균 증식, Time: 0.03 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120910. 세균 증식/세균 증식.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int n, int t) {
+        int answer = 0; // 1,4  2,8  3,16
+        for(int i=0 ; i<t; i++){
+            n=(n*2); // n=4, 
+            answer=n;
+        }
+        return answer;
+    }
+}
