2da72c53d068735a09cd365ffcc33c5dee02c06f
전유림 <165138502+yourim08@users.noreply.github.com>
2024-08-21 21:58:45 +0900

    [level 0] Title: k의 개수, Time: 23.10 ms, Memory: 81.2 MB -BaekjoonHub

--- Java/프로그래머스/0/120887. k의 개수/k의 개수.java
+++ Java/프로그래머스/0/120887. k의 개수/k의 개수.java
@@ -1,14 +1,14 @@
 class Solution {
-    public int solution(int i, int j, int k) {
-        int answer = 0;
-        int a=0;
+    public int solution(int i, int j, int k) { 
+        int answer=0; 
+        int a=0; 
         String n=String.valueOf(i); // i를 문자열로 변환
-        char k1=(char)(k+48);
-        for(int m=i; m<=j; m++){ //12번 돌음
+        char k1=(char)(k+48);       // 48을 더해서 숫자로 만든다.
+        for(int m=i; m<=j; m++){    // 12번 돌음
+            n=String.valueOf(m);
             for(int l=0; l<n.length(); l++){
                 if(n.charAt(l)==k1) answer++;
-                }
-            n=String.valueOf(m+1);
+            }
         }
         return answer;
     }
