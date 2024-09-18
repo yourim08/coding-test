ed5d151242d7356654f1b6828c699e72ab996850
전유림 <165138502+yourim08@users.noreply.github.com>
2024-09-18 22:16:15 +0900

    [level 0] Title: 로그인 성공?, Time: 0.03 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120883. 로그인 성공？/로그인 성공？.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String solution(String[] id_pw, String[][] db) {
+        String answer = "fail";
+        for(int i=0; i<db.length ;i++){
+            if(db[i][0].equals(id_pw[0])){
+                if(db[i][1].equals(id_pw[1])){
+                    answer="login";
+                    break;
+                }
+            }
+            if(db[i][0].equals(id_pw[0])){
+                    answer="wrong pw";
+                }     
+            }
+        return answer;
+    }
+}
