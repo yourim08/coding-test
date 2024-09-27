3192302b2e7db55d2056c19a108489806cc66e4a
전유림 <165138502+yourim08@users.noreply.github.com>
2024-09-27 11:05:35 +0900

    [level 0] Title: 각도기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/0/120829. 각도기/각도기.js
@@ -0,0 +1,8 @@
+function solution(angle) {
+    var answer = 0;
+    if(0<angle && angle<90) answer=1;
+    if(90<angle && angle<180) answer=3;
+    if(angle==90) answer=2;
+    if(angle==180) answer=4;
+    return answer;
+}
