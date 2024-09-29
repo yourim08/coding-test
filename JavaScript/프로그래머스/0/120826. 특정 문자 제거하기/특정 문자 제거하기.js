f6e233e446ad593c277fa4a1a7d0cf3743518bfc
전유림 <165138502+yourim08@users.noreply.github.com>
2024-09-29 20:11:32 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
@@ -0,0 +1,9 @@
+function solution(my_string, letter) {
+    var answer = '';
+    for(var i =0; i<my_string.length; i++){
+        if(my_string.charAt(i)!=letter){
+            answer+=my_string.charAt(i);
+        }
+    }
+    return answer;
+}
