d245fd560f87f551df6c0d8b896150e744773f99
전유림 <165138502+yourim08@users.noreply.github.com>
2024-09-29 20:59:17 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120811. 중앙값 구하기/README.md
+++ JavaScript/프로그래머스/0/120811. 중앙값 구하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 중앙값 구하기 - 120811 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120811?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120811) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.38 ms
+메모리: 33.4 MB, 시간: 0.08 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 29일 20:21:30
+2024년 09월 29일 20:59:13
 
 ### 문제 설명
 
--- JavaScript/프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.js
+++ JavaScript/프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.js
@@ -1,14 +1,15 @@
 function solution(array) {
     var answer = 0;
     var temp=0;
-    for(var i=0; i<array.length ;i++){
-        for(var j=i+1; j<array.length; j++)
-        if(array[i]>array[j]){
-            temp=array[i];
-            array[i]=array[j];
-            array[j]=temp;
-        }
-    }
+    // for(var i=0; i<array.length ;i++){
+    //     for(var j=i+1; j<array.length; j++)
+    //     if(array[i]>array[j]){
+    //         temp=array[i];
+    //         array[i]=array[j];
+    //         array[j]=temp;
+    //     }
+    // }
+    array.sort((a,b)=>a-b);
     answer=array[Math.floor(array.length/2)];
     return answer;
 }
