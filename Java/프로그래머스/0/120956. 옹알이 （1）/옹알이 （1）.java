37fad843f3661d5c721f4d7a4249f8d36c64459d
전유림 <165138502+yourim08@users.noreply.github.com>
2024-10-09 02:57:51 +0900

    [level 0] Title: 옹알이 (1), Time: 1.00 ms, Memory: 84.2 MB -BaekjoonHub

--- Java/프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
+++ Java/프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -1,48 +1,24 @@
 class Solution {
     public int solution(String[] babbling) {
-        int answer = 0;
-        String str[] = new String[340];
-		String bab[] = {"aya","ye","woo","ma"};
-		int n=0;
-        // 1개
-        for(int i=0; i<bab.length; i++){
-            str[n]=bab[i];
-            n++;
-        }
-		// 2개
-		for(int i=0; i<bab.length ; i++) {
-			for(int j=0; j<bab.length; j++) {
-				str[n]=bab[i]+bab[j];
-				n++;
-			}
-		}
-		// 3개
-		for(int i=0; i<bab.length ; i++) {
-			for(int j=0; j<bab.length; j++) {
-				for(int k=0; k<bab.length; k++) {
-					str[n]=bab[i]+bab[j]+bab[k];;
-					n++;
-				}
-			}
-		}
-		// 4개
-		for(int i=0; i<bab.length ; i++) {
-			for(int j=0; j<bab.length; j++) {
-				for(int k=0; k<bab.length; k++) {
-					for(int l=0; l<bab.length ; l++) {
-						str[n]=bab[i]+bab[j]+bab[k]+bab[l];
-						n++;
-					}
-				}
-			}
-		}
-        // 결과
-        for(int i=0; i<babbling.length; i++){
-            for(int j=0; j<str.length; j++){
-                if(babbling[i].equals(str[j]))
-                   answer++;
+        int result = 0;  // 결과를 저장할 변수 초기화
+        String[] validWords = {"aya", "ye", "woo", "ma"};  // 유효한 단어 리스트
+        
+        // 입력 배열 babbling의 각 문자열을 순회
+        for (String babble : babbling) {
+            // 유효한 단어들을 순회하면서 문자열에서 제거
+            for (String word : validWords) {
+                // 문자열에 유효한 단어가 포함되어 있으면
+                if (babble.indexOf(word) > -1) {
+                    // 유효한 단어를 빈칸으로 대체
+                    babble = babble.replace(word, " ");
+                }
+            }
+            // 모든 유효한 단어가 제거된 후, 문자열이 빈칸으로만 이루어져 있는지 확인
+            if (babble.trim().length() == 0) {
+                result++;  // 유효한 단어만 있는 경우 카운트 증가
             }
         }
-        return answer;
+        
+        return result;  // 최종 결과 반환
     }
 }
