caeff94bd2689e01558ffba670f1a857d678e7b8
Yourim Jeon <165138502+yourim08@users.noreply.github.com>
2024-11-08 22:11:29 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ C++/프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int slice, int n) {
+    int i=1;
+    int answer=0;
+        while (true){
+            if(slice*i>=n) {
+                answer=i;
+                break;
+            }
+            else i++;
+        }
+    return answer;
+}
