d8cfd605fd94ff004d5c1c29b24ced84c57fd552
Yourim Jeon <165138502+yourim08@users.noreply.github.com>
2024-11-11 21:43:03 +0900

    [level 0] Title: 팩토리얼, Time: 0.01 ms, Memory: 3.62 MB -BaekjoonHub

---
+++ C/프로그래머스/0/120848. 팩토리얼/팩토리얼.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int n) {
+    int answer = 0;
+    int m=1;
+    int k=1;
+    int j=2;
+    while(true){
+        k*=j;
+        if(k>n) {
+            answer=(j-1);
+                break;
+            }
+        else j++;
+        }
+    return answer;
+}
