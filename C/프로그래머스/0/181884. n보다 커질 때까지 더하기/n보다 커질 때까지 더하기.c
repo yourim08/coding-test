0da66d94ef5f47d7a504fb5d4a19148fdc89b85c
Yourim Jeon <165138502+yourim08@users.noreply.github.com>
2024-11-12 22:13:58 +0900

    [level 0] Title: n보다 커질 때까지 더하기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ C/프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// numbers_len은 배열 numbers의 길이입니다.
+int solution(int numbers[], size_t numbers_len, int n) {
+    int answer = 0;
+    int sum=0;
+        for(int i=0; i<numbers_len; i++){
+            if(sum>n) {
+                answer=sum;
+                break;
+            } 
+            else {
+                sum+=numbers[i];
+                answer=sum;
+                }
+        }
+    return answer;
+}
