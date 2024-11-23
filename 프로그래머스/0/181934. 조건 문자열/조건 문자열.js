2a11fe71a748b500f8c4baf11ce2c53a05ffecad
Yourim Jeon <165138502+yourim08@users.noreply.github.com>
2024-11-23 09:04:23 +0900

    [level 0] Title: 조건 문자열, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.js
@@ -0,0 +1,16 @@
+function solution(ineq, eq, n, m) {
+    var answer = 0;
+       if(ineq===">" && eq==="="){
+            if(n>=m) answer=1;
+        }
+        else if(ineq==="<" && eq==="="){
+            if(n<=m) answer=1;
+        }
+        else if(ineq===">" && eq==="!"){
+            if(n>m) answer=1;
+        }
+        else if(ineq==="<" && eq==="!"){
+            if(n<m) answer=1;
+        }
+    return answer;
+}
