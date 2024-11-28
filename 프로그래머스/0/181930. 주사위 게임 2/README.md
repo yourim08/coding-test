f61c46d42bcb9ea1f0ae78bd51c88207722c36f3
Yourim Jeon <165138502+yourim08@users.noreply.github.com>
2024-11-28 23:48:12 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.33 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/README.md
+++ 프로그래머스/0/181930. 주사위 게임 2/README.md
@@ -1,10 +1,10 @@
 # [level 0] 주사위 게임 2 - 181930 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181930?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181930?language=csharp) 
 
 ### 성능 요약
 
-메모리: 3.67 MB, 시간: 0.01 ms
+메모리: 31.4 MB, 시간: 0.33 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 28일 23:47:12
+2024년 11월 28일 23:48:10
 
 ### 문제 설명
 
---
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Solution {
+    public int solution(int a, int b, int c) {
+        int answer = 0;
+        if(a!=b && b!=c && a!=c) answer= a+b+c;
+        else if(a==b && a!=c) answer=(a+b+c)*(a*a+b*b+c*c);
+        else if(b==c && c!=a) answer=(a+b+c)*(a*a+b*b+c*c);
+        else if(a==c && a!=b) answer=(a+b+c)*(a*a+b*b+c*c);
+        else answer=(a+b+c)*(a*a+b*b+c*c)*(a*a*a+b*b*b+c*c*c);
+        return answer;
+    }
+}
