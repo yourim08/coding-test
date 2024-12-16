73e6859989c17ef52b642388dcc31e09fed60bb0
Yourim Jeon <165138502+yourim08@users.noreply.github.com>
2024-12-16 18:07:25 +0900

    [level 0] Title: 개미 군단, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int hp) {
+    int answer = 0;
+    int ant=1;
+    int sant=3;
+    int cant=5;
+    int count=(hp/cant)+((hp%cant)/sant)+(((hp%cant)%sant)/ant);
+    answer=count;
+    return answer;
+}
