3c3b9f362cfdc58d23c3fc2289d1914981575867
Yourim Jeon <165138502+yourim08@users.noreply.github.com>
2025-01-14 11:10:43 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/README.md
+++ 프로그래머스/1/12943. 콜라츠 추측/README.md
@@ -1,10 +1,10 @@
 # [level 1] 콜라츠 추측 - 12943 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12943) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12943?language=javascript) 
 
 ### 성능 요약
 
-메모리: 92.3 MB, 시간: 0.07 ms
+메모리: 33.5 MB, 시간: 0.16 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 12일 20:49:32
+2025년 01월 14일 11:10:41
 
 ### 문제 설명
 
---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,20 @@
+function solution(num) {
+    var answer = 0;
+    while(true){
+        if(answer == 500){
+            return -1;
+        }
+        if(num == 1){
+            break;
+        }
+        else if(num%2==0){
+            num/=2;
+            answer++;
+        }
+        else if(num%2!=0) {
+            num = num*3+1;
+            answer++;
+        }
+    }
+    return answer;
+}
