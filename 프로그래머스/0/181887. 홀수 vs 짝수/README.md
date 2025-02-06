8ca3a66a274e0c6f568f9558b050407b4dbde52a
Yourim Jeon <165138502+yourim08@users.noreply.github.com>
2025-02-06 23:45:14 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.30 ms, Memory: 31 MB -BaekjoonHub

--- 프로그래머스/0/181887. 홀수 vs 짝수/README.md
+++ 프로그래머스/0/181887. 홀수 vs 짝수/README.md
@@ -1,10 +1,10 @@
 # [level 0] 홀수 vs 짝수 - 181887 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181887?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181887?language=csharp) 
 
 ### 성능 요약
 
-메모리: 33.1 MB, 시간: 0.12 ms
+메모리: 31 MB, 시간: 0.30 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 23일 09:02:12
+2025년 02월 06일 23:45:11
 
 ### 문제 설명
 
---
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        int oddsum = 0;
+        int evensum = 0;
+        for(int i=0 ; i< num_list.Length ;i++){
+            if(i%2==0) oddsum+=num_list[i];
+            else evensum+=num_list[i];
+        }
+        if(oddsum>evensum) answer=oddsum;
+        else answer=evensum;
+        return answer;
+    }
+}
