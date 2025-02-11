0ae0e13e86812a8fe6bc3f2baa33ae96d6014d42
Yourim Jeon <165138502+yourim08@users.noreply.github.com>
2025-02-11 20:24:09 +0900

    [level 0] Title: 배열 자르기, Time: 0.26 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120833. 배열 자르기/배열 자르기.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Solution {
+    public int[] solution(int[] numbers, int num1, int num2) {
+        int[] answer = new int[num2-num1+1];
+        for(int i=0; i< answer.Length ;i++){
+            answer[i]=numbers[num1+i];
+        }
+        return answer;
+    }
+}
