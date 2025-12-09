e8013de03fc8f3fd64c590145f6bb9f3826525f0
Yourim Jeon <165138502+yourim08@users.noreply.github.com>
2025-12-09 23:12:31 +0900

    [Bronze V] Title: 사분면 고르기, Time: 164 ms, Memory: 17660 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class Main{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        if(a>0 && b>0){
+            System.out.print("1");
+        }
+        else if(a<0 && b>0){
+            System.out.print("2");
+        }
+        else if(a<0 && b<0){
+            System.out.print("3");
+        }
+        else if(a>0 && b<0){
+            System.out.print("4");
+        }
+    }
+}
