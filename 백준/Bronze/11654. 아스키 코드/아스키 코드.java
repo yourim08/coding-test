98f8afc29c0b542cda358a2b2b985ccafcc5247c
Yourim Jeon <165138502+yourim08@users.noreply.github.com>
2026-01-26 01:06:58 +1100

    [Bronze V] Title: 아스키 코드, Time: 176 ms, Memory: 17692 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+class Main{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        System.out.print((byte)(s.charAt(0)));
+    }
+}
