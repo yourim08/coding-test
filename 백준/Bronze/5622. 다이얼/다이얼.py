f0ad9217d8a7af3628c260ddab6de6b034ec3e38
Yourim Jeon <165138502+yourim08@users.noreply.github.com>
2026-02-24 23:47:09 +0900

    [Bronze II] Title: 다이얼, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/5622. 다이얼/다이얼.py
@@ -0,0 +1,9 @@
+n = input()
+check = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+number = [3,3,3,4,4,4,5,5,5,6,6,6,7,7,7,8,8,8,8,9,9,9,10,10,10,10]
+result = 0
+
+for i in n:
+    result += number[check.find(i)]
+    
+print(result)
