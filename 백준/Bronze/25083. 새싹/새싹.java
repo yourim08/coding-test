724be0be8c183178281d3ee286f9091ebfdb5e22
Yourim Jeon <165138502+yourim08@users.noreply.github.com>
2026-02-27 23:34:42 +0900

    [Bronze V] Title: 새싹, Time: 92 ms, Memory: 13956 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,5 @@
+class Main {
+    public static void main(String args[]){
+        System.out.print("         ,r'\"7"+"\n"+"r`-_   ,'  ,/"+"\n"+" \\. \". L_r'"+"\n"+"   `~\\/"+"\n"+"      |\n"+"      |\n");
+    }
+}
