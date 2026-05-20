ee3e3933bbf57d00c86d11af3cde21ff1bc28890
Yourim Jeon <165138502+yourim08@users.noreply.github.com>
2026-05-20 15:07:10 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.12 ms, Memory: 32.4 MB -BaekjoonHub

--- 프로그래머스/0/181919. 콜라츠 수열 만들기/README.md
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 콜라츠 수열 만들기 - 181919 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181919?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181919?language=javascript) 
 
 ### 성능 요약
 
-메모리: 9.15 MB, 시간: 0.02 ms
+메모리: 32.4 MB, 시간: 0.12 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 05월 20일 15:04:42
+2026년 05월 20일 15:07:07
 
 ### 문제 설명
 
--- 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
@@ -1,26 +1,8 @@
 function solution(n) {
-    let count=0;
-    let origin=n;
-    while (true){
-        if(n==1) break;
-        if(n%2==0) {
-            n=n/2;
-            count++;
-        }
-        else {
-            n=n*3+1;
-            count++;
-        }
-    }
-    var answer = new Array(count+1);
-    answer[0]=origin;
-    for(let i=1 ; i<answer.length;i++){
-        if(answer[i-1]%2==0) {
-            answer[i]=answer[i-1]/2;
-        }
-        else {
-            answer[i]=answer[i-1]*3+1;
-        }
+    let answer = [n];
+    while (n!=1){
+        n = (n%2==0) ? (n/2) : (n*3+1);
+        answer.push(n)
     }
     return answer;
 }
