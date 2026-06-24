39dd8a2d44a0e864b83b0131b1500ca328f73a51
Yourim Jeon <165138502+yourim08@users.noreply.github.com>
2026-06-24 23:36:52 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.00 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.py
+++ 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.py
@@ -2,6 +2,5 @@ def solution(my_string, n):
     answer = ''
     m = 0
     for i in range(len(my_string)) :
-        for j in range(n) :
-            answer += my_string[i]
+        answer += my_string[i]*n
     return answer
