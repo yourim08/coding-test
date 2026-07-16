313e6b5c7a76bbcacfb793244daaa944a5e84d2c
Yourim Jeon <165138502+yourim08@users.noreply.github.com>
2026-07-16 10:44:20 +0900

    [level 0] Title: 그림 확대, Time: 7.63 ms, Memory: 88.7 MB -BaekjoonHub

--- 프로그래머스/0/181836. 그림 확대/그림 확대.java
+++ 프로그래머스/0/181836. 그림 확대/그림 확대.java
@@ -6,13 +6,13 @@ class Solution {
             answer[n] = "";
             for(int j=0; j<picture[i].length(); j++){
                 for(int l=0; l<k; l++){
-                    answer[n]+=picture[i].charAt(j); 
+                    answer[n] += picture[i].charAt(j); 
                 }
                 for(int h=1; h<k; h++){
                     answer[n+h] = answer[n];
                 }
             }
-            n+=k;
+            n += k;
         }
         return answer;
     }
