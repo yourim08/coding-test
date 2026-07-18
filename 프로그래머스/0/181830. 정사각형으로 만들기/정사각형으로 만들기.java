81c1f3373fd41005f47ee986352254c4b75a497d
Yourim Jeon <165138502+yourim08@users.noreply.github.com>
2026-07-19 01:20:51 +0900

    [level 0] Title: 정사각형으로 만들기, Time: 0.27 ms, Memory: 90.3 MB -BaekjoonHub

--- 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.java
+++ 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.java
@@ -1,12 +1,10 @@
 class Solution {
     public int[][] solution(int[][] arr) {
-        int row = arr[0].length;
-        int col = arr.length;
-        int stan = (row>col) ? row : col;
+        int stan = (arr[0].length>arr.length) ? arr[0].length : arr.length;
         int[][] answer = new int[stan][stan];
         for(int i=0; i<arr.length; i++){
             for(int j=0; j<arr[i].length; j++){
-                answer[i][j] = arr[i][j];
+                answer[i][j]=arr[i][j];
             }
         }
         return answer;
