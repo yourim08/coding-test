9ab56d62a3c7629bbd1e6cc76344cecc19479e7f
Yourim Jeon <165138502+yourim08@users.noreply.github.com>
2026-08-02 19:24:16 +0900

    [level 0] Title: 세 개의 구분자, Time: 31.16 ms, Memory: 82.8 MB -BaekjoonHub

--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.js
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.js
@@ -1,7 +1,4 @@
 function solution(myStr) {
-    var answer = myStr.split(/[abc]/).filter(e => e);
-    if(answer.length == 0){
-        answer = ["EMPTY"]
-    }
+    var answer = myStr.split(/[abc]/).filter((e) => e).length != 0 ? myStr.split(/[abc]/).filter(e => e) : ["EMPTY"];
     return answer;
 }
